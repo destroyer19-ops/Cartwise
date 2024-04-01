20798f0d588ee9f4fc93854a9480e173f97e50c5
destroyer19-ops <agamahalvin@gmail.com>
2024-04-01 14:39:53 +0100

    first deploy

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='font-bold text-3xl underline'>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
